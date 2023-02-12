5bcd63a449b7b88197c1fcd8b61e08bf51a570a1
kassimoussa <kassim.moussali@gmail.com>
2023-02-12 11:03:25 +0300

    vf

--- resources/views/1/listes/new_fiches.blade.php
+++ resources/views/1/listes/new_fiches.blade.php
@@ -85,7 +85,7 @@
                                                 data-bs-toggle="tooltip" data-bs-placement="bottom" title="Voir la fiche ">
                                                 <i class="fas fa-eye"></i> Voir la fiche
                                             </a>
-                                            @if (session('level') == 3 || session('level') == 4)
+                                            @if (session('level') == 1 || session('level') == 3 || session('level') == 4)
                                                 <button type="button" class="btn btn-link dropdown-item"
                                                     data-bs-toggle="modal" data-bs-target="#{{ $reassignermodal }}">
                                                     <i class="fas fa-share-alt" data-bs-toggle="tooltip"
